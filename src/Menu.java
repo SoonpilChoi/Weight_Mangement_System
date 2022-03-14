e77ca612e2e28d098e16f269d8a7162afddad1e8
csp09 <csp09@DESKTOP-43GJMJH>
2022-03-14 22:08:22 +0900

    메뉴 정함

---
+++ src/Menu.java
@@ -0,0 +1,14 @@
+
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println("1.오늘 몸무게");
+		System.out.println("2.아침식사");
+		System.out.println("3.점심식사");
+		System.out.println("4.저녁식사");
+		System.out.println("5.간식이나 야식");
+		System.out.println("6.운동량");
+
+	}
+
+}
